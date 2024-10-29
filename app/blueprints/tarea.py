67b0f9938a2dc7e476c8723294512839b5c93df3
simperiale <ssimperiale@gmail.com>
2024-10-29 08:48:41 -0300

    uwsgi

--- app/blueprints/tarea.py
+++ app/blueprints/tarea.py
@@ -3,7 +3,7 @@ from schemas.schemas import TipoTareaIn, TareaGetIn, TipoTareaOut, TareaIn, Tare
 from schemas.schemas import SubtipoTareaIn, SubtipoTareaOut, SubtipoTareaCountOut, SubtipoTareaGetIn, SubtipoTareaPatchIn, TipoTareaPatchIn, TareaxGrupoIdOut
 from models.tarea_model import get_all_tarea, get_all_tarea_detalle, get_all_tipo_tarea, get_tarea_by_id, insert_tipo_tarea, usuarios_tarea, insert_tarea, delete_tarea, insert_usuario_tarea, delete_tipo_tarea, update_tarea
 from models.tarea_model import update_tipo_tarea, update_subtipo_tarea, get_all_subtipo_tarea, insert_subtipo_tarea, delete_subtipo_tarea, get_tarea_grupo_by_id
-from common.error_handling import DataError, DataNotFound, ValidationError
+from common.error_handling import DataError, DataNotFound, ValidationError, UnauthorizedError
 from models.alch_model import Usuario, Rol
 #from flask_jwt_extended import jwt_required
 from apiflask import APIBlueprint
@@ -25,10 +25,11 @@ def before_request():
         print("Token o api key no validos")
 
 ######################Control de acceso######################
-def control_rol_usuario(token='', nombre_usuario='', rol='', url_api=''):
+def control_rol_usuario(token='', nombre_usuario='SIMPERIALE', rol='', url_api=''):
     session: scoped_session = current_app.session
 
-    tiempo_vencimiento = timedelta(minutes=30)
+    #tiempo_vencimiento = timedelta(minutes=30)
+    tiempo_vencimiento = timedelta(days=360)
     query_usr = session.query(Usuario).filter(Usuario.email == nombre_usuario).first()
     if query_usr is None:
         print("Usuario no encontrado")
@@ -99,7 +100,12 @@ def control_rol_usuario(token='', nombre_usuario='', rol='', url_api=''):
             print("No tiene permisos")
             return False
         else:
-            print("Roles de tareas:",query_rol)
+            print("#"*50)
+            print("Roles de tareas:")
+            for permiso in query_rol:
+                print(f" Email: {permiso.email}, URL API: {permiso.url_api}")
+            
+            print("#"*50)
             return True
             
     
@@ -392,13 +398,14 @@ def get_tareas(query_data: dict):
 @tarea_b.output(TareaCountAllOut)
 def get_tareas_detalle(query_data: dict):
     try:
+        print("ENTRANDO A GET TAREAS")
         ##########Variables de control de acceso####################
         token='eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJsTVhjMjlEdmFtdEZhZ2tiUWoxQ2Fib1pqSk9OY1lxTE5teE1CWERIZmx3In0.eyJleHAiOjE3MjcyNjMzOTEsImlhdCI6MTcyNTk2OTA1OCwiYXV0aF90aW1lIjoxNzI1OTY3MzkxLCJqdGkiOiJhZDU5MzlhNS03NjkwLTQwNmUtYmZiMi1hNWM1OTMzZTg1MzciLCJpc3MiOiJodHRwczovL2Rldi1hdXRoLnBqbS5nb2IuYXIvYXV0aC9yZWFsbXMvZGV2b3BzIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6Ijg5MWFiOTA5LTMzNzQtNDU2YS04Y2U5LTdiZDJkZjExOWQwYSIsInR5cCI6IkJlYXJlciIsImF6cCI6InVzaGVyIiwibm9uY2UiOiI2MGE5ZGRlZS01ZGY3LTQxNjYtYjk3Ny0zMTBhZDk1MWI3NmIiLCJzZXNzaW9uX3N0YXRlIjoiYTFiMDA2NWQtMzhiOC00YTFmLTk2YzAtMTg0NmU0MzliY2VjIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJ1c2hlciI6eyJyb2xlcyI6WyJhZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsInNpZCI6ImExYjAwNjVkLTM4YjgtNGExZi05NmMwLTE4NDZlNDM5YmNlYyIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IlNpbHZpYSBTLiBJbXBlcmlhbGUiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzaW1wZXJpYWxlIiwiZ2l2ZW5fbmFtZSI6IlNpbHZpYSBTLiIsImZhbWlseV9uYW1lIjoiSW1wZXJpYWxlIiwiZW1haWwiOiJzaW1wZXJpYWxlQG1haWwuanVzLm1lbmRvemEuZ292LmFyIn0.OnfYhSdrZfz7bL6r1sy3-6DCTeFH8G7VesMbGWh4XGCiFM2IuqpXldrjlhKWI8ahRTKAJEdvXlBn8ht5JtGY1y-ee8RbeVrxtmjmmBHJt-nejXNflhsoXcF_20r3rMyfvM210vtFaUy26YZi7ttIBS6mQaql4Y_DPgL_wAMVoa431ThaDw3Kijcl7nJQ40fBeti0YgiwS3KKvEamf8E-CbX1gCUNoZX_pyP4dWSh9kduNh_K0QU4uqyvVzwyt8_jikcPxWmHQ9SHh31M5_31b6uEgSXx7QqKECs-VzH4GnkdZB3TwRb7fnj0D4jSuSvuUp5Wk_lPcrdNMDX4RxA84w'
         nombre_usuario='simperiale@mail.jus.mendoza.gov.ar'
         url_api='get/tarea'
         rol='administrador'
-        #accede = control_rol_usuario(token, nombre_usuario, rol, url_api)
-        accede = True
+        accede = control_rol_usuario(token, nombre_usuario, rol, url_api)
+        #accede = True
         if accede is False:
             raise DataError(800, "No tiene permisos para acceder a la API")
         #############################################################
