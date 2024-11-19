8a698574806e2c250ad2febc00c152a0e52193ed
simperiale <ssimperiale@gmail.com>
2024-11-19 12:38:09 -0300

    user_default

--- instala_sops.sh
+++
@@ -1,2 +0,0 @@
-curl -LO https://github.com/mozilla/sops/releases/download/v3.5.0/sops-v3.5.0.linux && mv ./sops-v3.5.0.linux /usr/local/bin/sops && chmod 0755 /usr/local/bin/sops
-gpg --import gestion-expte-sops.gpg
